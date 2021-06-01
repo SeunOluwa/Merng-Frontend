8709a97fe5c50caa39d1ad4bb218be31546de67e
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-06-01 16:23:03 +0100

    added proxy and edited sever base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,7 +6,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://mysterious-wave-49064.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
